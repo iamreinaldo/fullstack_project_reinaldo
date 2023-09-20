b3d8e58c1dee796cbbffe24ece8e2149424e8161
iamreinaldo <iamreinaldoneto@icloud.com>
2023-09-19 21:39:37 -0300

    estados, on blur, componente search

---
+++ test/src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,9 @@
+import livro from '../../imagens/livro.png'
+
+export const livros = [
+    {nome: 'Liderança', id: 1, src:livro},
+    {nome: 'JavaScritp', id: 3, src:livro},
+    {nome: 'Linux', id: 4, src:livro},
+    {nome: 'Python', id: 5, src:livro},
+]
+
